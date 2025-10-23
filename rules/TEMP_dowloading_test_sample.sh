6ada6876b3763fc83557a6b1278d03394ed6243e
camille.le-corre <camille.le-corre@universite-paris-saclay.fr>
2025-10-23 11:18:51 +0200

    Dowloading fastq files + dowloading test sample (bash scripts)

---
+++ rules/TEMP_dowloading_test_sample.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+sudo apt install -y sra-toolkit
+
+SRR_FILE=SRR10510434
+
+# Output folder
+OUTDIR=~
+#OUTDIR=~/data/mydatalocal
+
+
+# Number of threads to use
+THREADS=4
+
+# Loop on each SRR
+echo "Downloading $SRR_FILE"
+    
+# Delete the existing file if there is one
+if [ -f "$OUTDIR/$SRR_FILE.fastq" ]; then
+    echo "Existing file detected: deleting $OUTDIR/$SRR_FILE.fastq"
+    rm "$OUTDIR/$SRR_FILE.fastq"
+fi
+
+# Start download
+fasterq-dump --threads $THREADS --progress --outdir "$OUTDIR" "$SRR_FILE"
+
+echo "Fastq file download complete"
