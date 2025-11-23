8b3d26f45498a111c39b62e1748ca51f74488276
Saïda Moussaeva <saida.moussaeva@etu-upsaclay.fr>
2025-11-23 15:14:08 +0100

    Replace old deseq2.R with new version

--- scripts/deseq2.R
+++ scripts/deseq2.R
@@ -1,108 +1,91 @@
 #!/usr/bin/env Rscript
 
-########################
-# Parse CLI arguments
-########################
+###############
+# Parse args
+###############
 args <- commandArgs(trailingOnly = TRUE)
 
 parse_args <- function(args) {
   out <- list()
   for (i in seq(1, length(args), by = 2)) {
     key <- args[i]
-    val <- args[i + 1]
+    val <- args[i+1]
     if (key == "--counts")  out$counts  <- val
     if (key == "--samples") out$samples <- val
     if (key == "--outdir")  out$outdir  <- val
+    if (key == "--vsd")     out$vsd     <- val
   }
   out
 }
 
-# This function parses Snakemake script : 
-# Rscript scripts/deseq2.R \
-# --counts output/counts/all_samples_counts.txt \
-# --samples metadata/samples.tsv \
-# --outdir results/deseq2
-# and creates R list with input/samples/output : 
-
 opt <- parse_args(args)
 counts_file  <- opt$counts
-# counts_file <- "output/counts/all_samples_counts.txt"
 samples_file <- opt$samples
-# samples_file <- metadata/samples.tsv
 outdir       <- opt$outdir
-# outdir <- "results/deseq2"
+vsd_file     <- opt$vsd
 
-
-# Creates DESeq2 result directory
 dir.create(outdir, showWarnings = FALSE, recursive = TRUE)
 
-########################
-# Load required packages
-########################
+###############
+# Libraries
+###############
 library(DESeq2)
-library(ggplot2)
-library(pheatmap)
-library(dplyr)
-library(rentrez)
-library(EnrichmentBrowser)
-library(tidyr)
-library(forcats)
-library(ggrepel)
 
-########################
-# Import featureCounts output
-########################
+###############
+# Load counts
+###############
 counts <- read.delim(counts_file, comment.char="#", check.names = FALSE)
-
-# extract raw counts
-cts <- as.matrix(counts[ , 7:ncol(counts)])
+cts <- as.matrix(counts[,7:ncol(counts)])
 rownames(cts) <- counts$Geneid
 
-########################
-# Import sample metadata
-########################
-# Expected format:
-# sample   condition
-# SRR001   Normal
-# SRR002   Persistent
 samples <- read.delim(samples_file, sep="\t", stringsAsFactors = FALSE)
+cts <- cts[, samples$sample]
 
-if (!all(samples$sample %in% colnames(cts))) {
-  stop("Sample names in samples.tsv do not match count matrix column names.")
-}
-
-# reorder columns to match counts file
-cts <- cts[ , samples$sample]
-
-# build colData
 coldata <- data.frame(condition = factor(samples$condition))
 rownames(coldata) <- samples$sample
-
-# set reference level
 coldata$condition <- relevel(coldata$condition, ref = "Normal")
 
-########################
-# DESeq2 analysis
-########################
+###############
+# DESeq2
+###############
 dds <- DESeqDataSetFromMatrix(
   countData = cts,
   colData   = coldata,
-  # Model the gene expression **as a function of** the variable 'condition'.
   design    = ~ condition
 )
 
 dds <- DESeq(dds)
 res <- results(dds)
 
-########################
-# Convert DESeqResults → data.frame
-########################
-
+###############
+# Dataframes
+###############
 res_df <- as.data.frame(res)
 res_df$Geneid <- rownames(res_df)
+rownames(res_df) <- res_df$Geneid
 res_df$signif <- !is.na(res_df$padj) & res_df$padj < 0.05
 
+deg_df <- res_df[res_df$signif, ]
+deg_up_df   <- deg_df[deg_df$log2FoldChange > 0, ]
+deg_down_df <- deg_df[deg_df$log2FoldChange < 0, ]
 
-write.csv(res_df, file.path(outdir,"deseq2_results.csv"), row.names=FALSE)
-saveRDS(dds,       file.path(outdir,"dds.rds"))
 
+rownames(deg_df)      <- deg_df$Geneid
+rownames(deg_up_df)   <- deg_up_df$Geneid
+rownames(deg_down_df) <- deg_down_df$Geneid
+
+
+###############
+# VST
+###############
+vsd <- vst(dds, blind = FALSE)
+
+###############
+# Write outputs
+###############
+write.csv(res_df,      file.path(outdir, "res_df.csv"),   row.names = TRUE)
+write.csv(deg_df,      file.path(outdir, "deg.csv"),      row.names = TRUE)
+write.csv(deg_up_df,   file.path(outdir, "deg_up.csv"),   row.names = TRUE)
+write.csv(deg_down_df, file.path(outdir, "deg_down.csv"), row.names = TRUE)
+
+saveRDS(vsd, vsd_file)
